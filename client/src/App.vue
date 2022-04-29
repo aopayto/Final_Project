aba1c2ab19da0178b075f3c13690e62ed192afc1
aopayto <aopayto@gmail.com>
2022-04-29 15:56:40 -0600

    Updated Fuctionality for YouTube Generator, Added Amazon Generator, updated page styling

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <img  src="./assets/logo.png" width="380" height="250">
+    <img  src="./assets/logo.png" style="width:380px; height:250px; float:left; padding-left:200px; margin-top:30px">
+    <img  src="./assets/amazon.png" style="width:380px; height:300px; float:right; padding-right:200px; margin-top:30px">
     <router-view/>
   </div>
 </template>
@@ -20,5 +21,63 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.button {
+  background-color: blue;
+  border: none;
+  color: blue;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+.button1 {
+  background-color:gray;
+  color: white;
+  border: 2px solid black;
+  border-radius: 12px;
+}
+
+.button1:hover {
+  background-color: #f44336;
+  color: black;
+  border: 2px solid black;
+  border-radius: 12px;
+}
+.button2 {
+  background-color:gray;
+  color: white;
+  border: 2px solid black;
+  border-radius: 12px;
+}
+
+.button2:hover {
+  background-color: #19a5d8;
+  color: black;
+  border: 2px solid black;
+  border-radius: 12px;
+}
+header {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  text-align: center;
+  padding: 3px;
+  margin-bottom: 20px;
+  background-color: rgb(62, 151, 199);
+  color: white;
+}
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  padding: 3px;
+  background-color: rgb(62, 151, 199);
+  color: white;
 }
 </style>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,61 @@
+
+<template>
+  <div>
+  <table style="width:100%">
+  <tr>
+  <td>
+    <h1>Random YouTube Video Generator</h1>
+    <button class='button button1' @click='generate'>Generate</button>
+  </td>
+  <td>
+    <h1>Random Amazon Listing Generator</h1>
+    <button class='button button2' @click='aznGenerate'>Generate</button>
+  </td>
+  </tr>
+</table>
+  </div>
+</template>
+
+<script>
+import genamazon from '@/services/genamazon'
+import generate from '@/services/generate'
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    generate () {
+      let url = new URL(generate.generate)
+      console.log(url)
+      window.open(url, '_blank')
+      window.location.reload()
+    },
+    aznGenerate () {
+      let aznUrl = new URL(genamazon.genamazon)
+      console.log(aznUrl)
+      window.open(aznUrl, '_blank')
+      window.location.reload()
+    }
+  }
+}
+
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
